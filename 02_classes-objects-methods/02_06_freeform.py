87d5b94d204abfea686c909cca18247ee1a4877e
tianlin <tlin5@babson.edu>
2022-01-22 20:27:54 -0500

    add 0206

--- 02_classes-objects-methods/02_06_freeform.py
+++ 02_classes-objects-methods/02_06_freeform.py
@@ -10,3 +10,56 @@
 # Be creative. Have some fun. :)
 # Using objects you can model anything you want:
 # Animals, paintings, card games, sports teams, trees, people etc...
+
+class Gun:
+
+    def __init__(self, model, round, chamber):
+        self.model = model
+        self.round = round
+        self.chamber = chamber
+
+    def __add__(self, other):
+        other.current_round -= 1
+        self.chamber = 1
+        self.round = other.current_round
+        return Gun(self.model, self.round, self.chamber)
+    
+    def __str__(self):
+        return f'This gun model is {self.model} with {self.round} round loaded and {self.chamber} round in the chamber.'
+
+class Magazine:
+
+    def __init__(self, magazine_size, model, current_round):
+        self.magazine_size = magazine_size
+        self.model = model
+        self.current_round = current_round
+    
+    def __add__(self, other):
+        """This is add func is for magazine and ammo"""
+        self.current_round += self.magazine_size
+        other.ammo_count -= self.magazine_size
+        return Magazine(self.magazine_size, self.model, self.current_round)
+
+    def __str__(self):
+        return f'This magazine can hold {self.magazine_size} round of ammo. It is for {self.model} gun. This magazine currently has {self.current_round} round.'
+
+class Ammo:
+
+    def __init__(self, ammo_count, caliber, type):
+        self.ammo_count = ammo_count
+        self.caliber = caliber
+        self.type = type
+
+    def __str__(self):
+        return f'There are {self.ammo_count} rounds of ammo. The caliber of the round is {self.caliber}. The type of ammo is {self.type}'
+    
+if __name__ == '__main__':
+    g = Gun('HK416', 30, 0)
+    m = Magazine(30, 'HK 416 Magazine', 0)
+    a = Ammo(210, 5.56, 'm855')
+
+    l1 = m + a
+    l2 = g + m
+
+    print(l1)
+    print(l2)
